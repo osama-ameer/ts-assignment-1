1ba98356b7edd21acbde0ac418b3852af3a233d7
osama-ameer <osamaameer15@gmail.com>
2023-03-30 20:46:06 +0500

    initial commit

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch('https://jsonplaceholder.typicode.com/users');
+    const data = yield response.json();
+    return data;
+});
+const renderTable = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield fetchData();
+    console.log(data);
+    const table = document.getElementById('table_body');
+    data.map((item) => {
+        const row = document.createElement('tr');
+        const id = document.createElement('td');
+        const name = document.createElement('td');
+        const email = document.createElement('td');
+        const phone = document.createElement('td');
+        id.textContent = item.id.toString();
+        name.textContent = item.name;
+        email.textContent = item.email.toString();
+        phone.textContent = item.phone.toString();
+        row.appendChild(id);
+        row.appendChild(name);
+        row.appendChild(email);
+        row.appendChild(phone);
+        table.appendChild(row);
+    });
+});
+renderTable();
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,44 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    phone: number;
+  }
+  
+  const fetchData = async (): Promise<User[]> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return data;
+  };
+  
+  const renderTable = async (): Promise<void> => {
+    const data = await fetchData();
+    console.log(data);
+    const table = document.getElementById('table_body')! as HTMLElement;
+    
+
+  data.map((item: User) => {
+    const row = document.createElement('tr');
+    const id = document.createElement('td');
+    const name = document.createElement('td');
+    const email = document.createElement('td');
+    const phone = document.createElement('td');
+
+
+    id.textContent = item.id.toString();
+    name.textContent = item.name;
+    email.textContent = item.email.toString();
+    phone.textContent = item.phone.toString();
+
+    row.appendChild(id);
+    row.appendChild(name);
+    row.appendChild(email);
+    row.appendChild(phone);
+
+
+    table.appendChild(row);
+  })
+             
+  };
+  
+  renderTable();
